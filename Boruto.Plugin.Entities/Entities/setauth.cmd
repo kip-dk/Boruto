b6c44bc26e3972adf2476b4489d6e4bc8a7d98b2
Kjeld Poulsen <kip@boruto.com>
2025-05-29 08:46:44 +0200

    div

--- Boruto.Plugin.Entities/Entities/setauth.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-call pac auth clear
-call pac auth create --name boruto --environment https://boruto-kipon.crm4.dynamics.com --applicationId 5b87d456-3618-4d4d-927f-c8bdd2440f53 --clientSecret "TwC8Q~DSPDqQQtdPAQ2F3IOGNTvReoZLSAv7odiG" --tenant ec4b2478-41f3-4a0b-b5cb-fd8017819f23
-call pac auth list
-	
