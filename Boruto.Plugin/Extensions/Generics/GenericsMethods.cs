33b45cad24cfe28304e0f9367b9b72f35c351d0e
Kjeld Poulsen <kip@boruto.com>
2025-07-08 10:46:17 +0200

    div

--- Boruto.Plugin/Extensions/Generics/GenericsMethods.cs
+++ Boruto.Plugin/Extensions/Generics/GenericsMethods.cs
@@ -16,5 +16,21 @@ namespace Boruto.Extensions.Generics
             }
             return null;
         }
+
+        public static IEnumerable<T[]> Pages<T>(this IEnumerable<T> input, int pageSize)
+        {
+            var result = new List<T[]>();
+
+            var arr = input.ToArray();
+
+            var next = arr.Take(pageSize).ToArray();
+            while (next.Length > 0)
+            {
+                result.Add(next);
+                arr = arr.Skip(pageSize).ToArray();
+                next = arr.Take(pageSize).ToArray();
+            }
+            return result;
+        }
     }
 }
