7bd71e06b47609b487b11f72f27b45d7ad2cffdd
Kjeld Poulsen <kip@boruto.com>
2025-08-05 15:00:41 +0200

    messages for handling members requests

---
+++ Boruto.Plugin/Messages/AddListMembersRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boruto.Messages
+{
+    public class RemoveListMembersRequest : Microsoft.Xrm.Sdk.OrganizationRequest
+    {
+        public RemoveListMembersRequest(): base ("RemoveListMembers")
+        {
+
+        }
+
+        public Guid ListId
+        {
+            get
+            {
+                return (Guid)this.Parameters[nameof(ListId)];
+            }
+            set
+            {
+                this.Parameters[nameof(ListId)] = value;
+            }
+        }
+
+        public Guid[] MemberIds
+        {
+            get
+            {
+                return (Guid[])this.Parameters[nameof(MemberIds)];
+            } set
+            {
+                this.Parameters[nameof(MemberIds)] = value;
+            }
+        }
+    }
+}
---
+++ Boruto.Plugin/Messages/RemoveMemberRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boruto.Messages
+{
+    public class RemoveMemberRequest : Microsoft.Xrm.Sdk.OrganizationRequest
+    {
+        public RemoveMemberRequest() : base("RemoveMember")
+        {
+
+        }
+
+        public Guid ListId
+        {
+            get
+            {
+                return (Guid)this.Parameters[nameof(ListId)];
+            }
+            set
+            {
+                this.Parameters[nameof(ListId)] = value;
+            }
+        }
+
+        public Guid EntityId
+        {
+            get
+            {
+                return (Guid)this.Parameters[nameof(EntityId)];
+            }
+            set
+            {
+                this.Parameters.Add(nameof(EntityId), value);
+            }
+        }
+    }
+}
