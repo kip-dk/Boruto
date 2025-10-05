ebd6545daa043676560bbe3b9b88d8de95755fe5
Kjeld Poulsen <kip@boruto.com>
2025-10-05 09:44:55 +0200

    div

--- Boruto.Plugin/Messages/AddListMembersRequest.cs
+++ Boruto.Plugin/Messages/AddListMembersRequest.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Microsoft.Xrm.Sdk;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI;
 
 namespace Boruto.Messages
 {
@@ -29,7 +31,28 @@ namespace Boruto.Messages
         {
             get
             {
-                return (Guid[])this.Parameters[nameof(MemberIds)];
+                if (this.Parameters.TryGetValue(nameof(MemberIds), out Guid[] mids))
+                {
+                    return mids;
+                }
+
+                if (Boruto.PluginContext.Current != null)
+                {
+                    var keys = this.Parameters.Keys;
+                    foreach (var key in keys)
+                    {
+                        var vale = this.Parameters[key];
+                        if (vale != null)
+                        {
+                            Boruto.Trace.Info($"{key} = {vale}, {vale.GetType().FullName}");
+                        }
+                        else
+                        {
+                            Boruto.Trace.Info($"{key} is nnull");
+                        }
+                    }
+                }
+                throw new InvalidPluginExecutionException($"Expected: { nameof(MemberIds) } to be in the request payload.");
             }
             set
             {
