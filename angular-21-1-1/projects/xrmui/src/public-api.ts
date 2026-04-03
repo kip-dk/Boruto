7d7828f8c99778f4ece46b943498ab8daa87438f
Kjeld Poulsen <kip@boruto.com>
2026-04-03 10:45:45 +0200

    div

--- angular-21-1-1/projects/xrmui/src/public-api.ts
+++ angular-21-1-1/projects/xrmui/src/public-api.ts
@@ -2,8 +2,6 @@
  * Public API Surface of xrmui
  */
 
-export * from './lib/xrmui';
-
 export * from './lib/ko/forms/kobuttongroup/kobuttongroup.component';
 export * from './lib/ko/forms/kodecimal.directive';
 export * from './lib/ko/forms/kofocus.directive';
