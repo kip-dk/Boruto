1764fef7694bb17826c4e612b1d3083185718afb
Kjeld Poulsen <kip@boruto.com>
2026-05-31 13:05:16 +0200

    div

--- angular-21-1-1/projects/xrm/src/lib/xrmcontext.service.ts
+++ angular-21-1-1/projects/xrm/src/lib/xrmcontext.service.ts
@@ -670,7 +670,7 @@ export class XrmContextService {
     let _instance = instance as IndexedObject;
 
     for (let prop in prototype) {
-      if (prototype.hasOwnProperty(prop) && typeof _prototype[prop] != 'function') {
+      if (prototype.hasOwnProperty(prop) && typeof _prototype[prop] != 'function' && _prototype[prop] != undefined) {
         if (prototype.ignoreColumn(prop)) continue;
         let prevValue = cm[prop];
         let newValue = _instance[prop];
@@ -986,7 +986,7 @@ export class XrmContextService {
     let _instance = instance as IndexedObject;
 
     for (let prop in prototype) {
-      if (prototype.hasOwnProperty(prop) && typeof _prototype[prop] !== 'function') {
+      if (prototype.hasOwnProperty(prop) && typeof _prototype[prop] !== 'function'  && _prototype[prop] != undefined) {
         if (prototype.ignoreColumn(prop)) continue;
 
         let value = _instance[prop];
@@ -1282,7 +1282,7 @@ export class XrmContextService {
     for (let prop in prototype) {
       if (prototype.ignoreColumn(prop)) continue;
 
-      if (prototype.hasOwnProperty(prop) && typeof prototype[prop] != 'function') {
+      if (prototype.hasOwnProperty(prop) && typeof prototype[prop] != 'function' && prototype[prop] != undefined) {
         let done = false;
         if (prototype[prop] instanceof EntityReference) {
           let ref = new EntityReference();
@@ -1419,7 +1419,7 @@ export class XrmContextService {
 
     for (let prop in prototype) {
       if (prototype.ignoreColumn(prop)) continue;
-      if (prototype.hasOwnProperty(prop) && typeof prototype[prop] != 'function') {
+      if (prototype.hasOwnProperty(prop) && typeof prototype[prop] != 'function' && prototype[prop] != undefined) {
         let v = instance[prop];
         if (v == null) continue;
 
