2fa511382cd1c96fb662f376780ebd060d4c4521
Kjeld Poulsen <kip@boruto.com>
2026-09-09 19:34:59 +0200

    div

--- angular-21-1-1/projects/boruto-test-app/src/app/app.routes.ts
+++ angular-21-1-1/projects/boruto-test-app/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { FirstpageComponent } from './components/firstpage/firstpage.component';
 import { KoComponent } from './components/ko/ko.component';
 import { BoComponent } from './components/bo/bo.component';
 import { LoComponent } from './components/lo/lo.component';
+import { PocComponent } from './components/poc/poc.component';
 
 export const routes: Routes = [
     { path: '', component: BoComponent },
     { path: 'ko', component: KoComponent },
     { path: 'bo', component: BoComponent },
     { path: 'lo', component: LoComponent },
-    { path: 'firstpage', component: FirstpageComponent }
+    { path: 'firstpage', component: FirstpageComponent },
+    { path: 'poc', component: PocComponent }
 ];
---
+++ angular-21-1-1/projects/boruto-test-app/src/app/components/poc/poc.component.ts
@@ -0,0 +1,11 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-poc',
+  templateUrl: './poc.component.html',
+  styleUrl: './poc.component.scss',
+  changeDetection: ChangeDetectionStrategy.Eager,
+  imports: []
+})
+export class PocComponent {
+}
